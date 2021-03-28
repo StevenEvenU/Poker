f84027ec89e86ec4f2cee1546d30781220df3452
Zachary Gelfond <zmg24@cornell.edu>
2021-03-28 19:54:14 -0400

    deck.mli added2

---
+++ deck.mli
@@ -0,0 +1,8 @@
+(**The deck of cards in order. Able to be shuffled.*)
+
+(** The abstract type of cards in order in a deck. *)
+type t
+
+(** The type of suits. *)
+type suit = Spades | Hearts | Diamonds | Clubs
+
--- table.mli
+++ table.mli
@@ -1,1 +1,2 @@
 (** The location of all cards, such as on the table, in players' hands, and in the deck. *)
+
