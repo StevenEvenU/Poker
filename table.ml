4eae2c82f78e1f4e3182f00596b0e7f92fb659a4
Zachary Gelfond <zmg24@cornell.edu>
2021-03-29 13:12:27 -0400

    made table work with changes to deck

--- table.ml
+++ table.ml
@@ -10,28 +10,35 @@ type state = {
   mutable turn: players;
 }
 
+
 let table_deck = create
 let init_state = {users_hand = []; cpu_hand = []; cards_on_table = []; deck_rem = table_deck; turn = Player}
+
+let remove (deck_rem: Deck.deck):Deck.card  = 
+let x = Deck.top_card deck_rem in 
+match x with 
+|Some x -> x
+(*| None ->*) 
 
 let delegate state = shuffle state.deck_rem;
   for i = 0 to 1 do
     state.cpu_hand <- (remove state.deck_rem :: state.cpu_hand);
-    state.deck_rem <- table_deck
+    state.deck_rem <- Deck.remove_top table_deck
   done;
   for i = 0 to 1 do
     state.users_hand <- (remove state.deck_rem :: state.users_hand); 
-    state.deck_rem <- table_deck
+    state.deck_rem <- Deck.remove_top table_deck
   done
 
 let deal state =
   for i = 0 to 2 do
     state.cards_on_table <- (remove state.deck_rem:: state.cards_on_table);
-    state.deck_rem <- table_deck
+    state.deck_rem <- Deck.remove_top table_deck
   done
 
 let flop state = 
   state.cards_on_table <- (remove state.deck_rem :: state.cards_on_table);
-  state.deck_rem <- table_deck
+  state.deck_rem <- Deck.remove_top table_deck
 
 let winner state = failwith "Not implemented"
 
