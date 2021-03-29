71e11c7afabf383dacbf7c4ea6f80aa03a658f68
Zachary Gelfond <zmg24@cornell.edu>
2021-03-28 20:44:32 -0400

    more updates to deck.ml and deck.mli

--- deck.ml
+++ deck.ml
@@ -18,10 +18,18 @@ type card ={suit : suit; value : value}
 
 type deck = card option array 
 
-let create = let card1 = createCard "Jack" "Hearts" in Array.make 52 card1
+let createCard valu sut = {suit = sut; value = valu}  
+
+let create : deck= 
+  let card1 = 
+    createCard Jack Hearts 
+  in 
+  Array.make 52 (Some card1)
 
 let shuffle = failwith "not"
 
 let remove = failwith "not"
 
 let size = failwith "not"
+
+let deck_to_string = failwith "not"
