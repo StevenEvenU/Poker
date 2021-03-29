9c1b149b8f4ac73e9d8bcc09b3edcb423eca65bc
Zachary Gelfond <zmg24@cornell.edu>
2021-03-28 22:35:32 -0400

    deck stuff done a bit

--- deck.ml
+++ deck.ml
@@ -28,9 +28,35 @@ type deck = card option array
 
 let createCard valu sut = { suit = sut; value = valu }
 
+let create_empty_helper : deck = Array.make 52 None
+
 let create : deck =
-  let card1 = createCard Jack Hearts in
-  Array.make 52 (Some card1)
+  let deck1 = create_empty_helper in
+  for y = 0 to 51 do
+    let sut =
+      if y < 13 then Hearts
+      else if y < 26 then Diamonds
+      else if y < 39 then Spades
+      else Clubs
+    in
+    let valu =
+      if y mod 13 = 0 then Two
+      else if y mod 13 = 1 then Three
+      else if y mod 13 = 2 then Four
+      else if y mod 13 = 3 then Five
+      else if y mod 13 = 4 then Six
+      else if y mod 13 = 5 then Seven
+      else if y mod 13 = 6 then Eight
+      else if y mod 13 = 7 then Nine
+      else if y mod 13 = 8 then Ten
+      else if y mod 13 = 9 then Jack
+      else if y mod 13 = 10 then Queen
+      else if y mod 13 = 11 then King
+      else Ace
+    in
+    deck1.(y) <- Some (createCard valu sut)
+  done;
+  deck1
 
 let shuffle = failwith "not"
 
