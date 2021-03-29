0b7c525c691dcb10e337f894b634814551a35bce
Zachary Gelfond <zmg24@cornell.edu>
2021-03-28 22:00:55 -0400

    updated deck.mli to remove to_string functions

--- deck.ml
+++ deck.ml
@@ -1,29 +1,35 @@
-type suit = Spades | Hearts | Diamonds | Clubs
+type suit =
+  | Spades
+  | Hearts
+  | Diamonds
+  | Clubs
 
-type value = Two 
-| Three 
-| Four 
-| Five 
-| Six 
-| Seven 
-| Eight 
-| Nine 
-| Ten 
-| Jack 
-| Queen 
-| King 
-| Ace
+type value =
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
 
-type card ={suit : suit; value : value} 
+type card = {
+  suit : suit;
+  value : value;
+}
 
-type deck = card option array 
+type deck = card option array
 
-let createCard valu sut = {suit = sut; value = valu}  
+let createCard valu sut = { suit = sut; value = valu }
 
-let create : deck= 
-  let card1 = 
-    createCard Jack Hearts 
-  in 
+let create : deck =
+  let card1 = createCard Jack Hearts in
   Array.make 52 (Some card1)
 
 let shuffle = failwith "not"
@@ -31,5 +37,3 @@ let shuffle = failwith "not"
 let remove = failwith "not"
 
 let size = failwith "not"
-
-let deck_to_string = failwith "not"
--- deck.mli
+++ deck.mli
@@ -1,40 +1,42 @@
 (** The deck of cards in order. Able to be shuffled.*)
 
 (** The type of suits. *)
-type suit = Spades | Hearts | Diamonds | Clubs
+type suit =
+  | Spades
+  | Hearts
+  | Diamonds
+  | Clubs
 
 (** The type of values. *)
-type value = Two 
-| Three 
-| Four 
-| Five 
-| Six 
-| Seven 
-| Eight 
-| Nine 
-| Ten 
-| Jack 
-| Queen 
-| King 
-| Ace
+type value =
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
 
 (** The abstract type of a single card. *)
-type card 
+type card
 
 (** The abstract type of cards in order in a deck.*)
-type deck  
+type deck
 
 (** Creates a shuffled deck of cards and returns it*)
 val create : unit -> deck
 
 (** Shuffles a deck (or smaller amount) of cards*)
-val shuffle: deck -> unit
+val shuffle : deck -> unit
 
 (** Removes the top card from the deck and gives the card*)
-val remove: deck -> card
+val remove : deck -> card
 
 (** Returns size of deck*)
-val size: deck -> int
-
-(** Returns string representing deck*)
-val deck_to_string: deck -> string
+val size : deck -> int
