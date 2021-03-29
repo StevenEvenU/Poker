1ddf952aa58034d675e9700fd8d9bdc1cf8c4e94
Steven Urdaneta <stevenaurdaneta@gmail.com>
2021-03-28 23:46:57 -0400

    Fixed table.ml

--- table.ml
+++ table.ml
@@ -1,31 +1,32 @@
 open Deck
-"Write in a function recursively that will keep on changing the init state based on how many players there are"
+(** Write in a function recursively that will keep on changing the init state based on how many players there are *)
+type players = Player | Computer
 
-let INIT_STATE = {
-  users_hand = []; 
-  cpu_hand = []; 
-  cards_on_table = []; 
-  deck_rem = create; 
-  turn: Player; 
+type state = {
+  users_hand: Deck.card list; 
+  cpu_hand: Deck.card list; 
+  cards_on_table: Deck.card list; 
+  deck_rem: Deck.deck;
+  turn: players;
 }
+let init_state = {users_hand = []; cpu_hand = []; cards_on_table = []; deck_rem = Deck.create (); turn = Player}
 
-let card_removed = remove
-
-let delegate state = shuffle;
+let delegate state = shuffle state.deck_rem;
   for i = 0 to 1 do
-    {state with cpu_hand = remove :: cpu_hand; deck_rem = deck}
-  done
+    {state with cpu_hand = (remove state.deck_rem :: state.cpu_hand); deck_rem = Deck.deck} 
+  done;
   for i = 0 to 1 do
-    {state with users_hand = remove :: users_hand; deck_rem = deck}
+    {state with users_hand = (remove state.deck_rem) :: state.users_hand; deck_rem = Deck.deck}
   done
 
 let deal state =
   for i = 0 to 2 do
-    {state with cards_on_table = remove :: cards_on_table; deck_rem = deck}
+    {state with cards_on_table = remove state.deck_rem:: state.cards_on_table; deck_rem = Deck.deck}
   done
 
-let flop state = 
-  {state with cards_on_table = remove :: cards_on_table; deck_rem = deck}
+let flop state = {state with cards_on_table = remove state.deck_rem :: state.cards_on_table; deck_rem = Deck.deck}
+
+let winner state = failwith "Not implemented"
 
 let round_check state = if List.length (state.cards_on_table) = 5 
   then winner state
