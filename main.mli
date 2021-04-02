180e450683f7d3138b4590e2447c44f31a713cf3
ejl242 <ejl242@cornell.edu>
2021-04-01 23:22:07 -0400

    Added TODOs and change descriptions

--- main.mli
+++ main.mli
@@ -4,7 +4,7 @@ type action = Check | Call | Raise | Fold
 (** Checks whether someone has raised or not *)
 val is_raised : State.state -> bool
 
-(** Gets the users next action *)
+(** Returns the users next action *)
 val get_action : State.state -> action
 
 (** Returns string of value *)
@@ -13,11 +13,11 @@ val string_of_value : Deck.value -> string
 (** Returns string of card *)
 val string_of_card : Deck.card -> string
 
-(** Returns string of card list *)
+(** Prints string of card list *)
 val string_of_cards : Deck.card list -> string
 
-(**  *)
+(** Prints a hand of cards *)
 val print_hand : Deck.card list -> State.players -> unit
 
-(** Returns string of the state *)
+(** Prints string of the state after a given event occurs*)
 val print_event : State.state -> string -> unit
