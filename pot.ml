97d25bcbfdfff536853be7772300008c901015bb
Zachary Gelfond <zmg24@cornell.edu>
2021-04-21 19:54:27 -0400

    added pot.cmo

---
+++ pot.ml
@@ -0,0 +1,7 @@
+open Compare
+
+type pot = int
+
+let add int = failwith "Unimplemented"
+
+let to_winner win_record list = failwith "unimplemented"
---
+++ pot.mli
@@ -0,0 +1,11 @@
+(** The money on the table*)
+open Compare
+
+(** The money on the table (the pot)*)
+type pot
+
+(** Add money to the pot*)
+val add : int -> pot
+
+(** Splits the pot amongs the winners. Uses a side pot if necessary.*)
+val to_winner : win_record list -> int list
