b926bbaaef79781fbe9da21961ab67eb4e906955
ejl242 <ejl242@cornell.edu>
2021-04-26 21:07:34 -0400

    Updates to betting function

--- table.ml
+++ table.ml
@@ -54,9 +54,10 @@ let flop state =
     | none -> state.cards_on_table);
   state.deck_rem <- remove_top state.deck_rem
 
-let rec betting state stop =
-  let under_the_gun = int_of_player state.dealer
-  in under_the_gun
+let bet (state : state) (amt : int) =
+  match state.turn with
+  | Player -> player_bet amt (state.user_money == amt)
+  | Computer x -> comp_bet (Computer x) false
 
 
 let winner (state : state) : win_record =
