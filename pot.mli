7e356c6dbf611631655c55183a47b7ab2d4ec0c0
Zachary Gelfond <zmg24@cornell.edu>
2021-04-28 20:50:27 -0400

    asfdjokh yaaa

--- pot.mli
+++ pot.mli
@@ -1,16 +1,14 @@
 (** The money on the table*)
-open Compare
-
 open State
 
 (** Add money to the pot. Must be given the state, the amount
     (IMPORTANT: give 0 if a player is calling, give -1 if a player is
     folding), and the player betting *)
-val add : int -> players -> unit
+val add : int -> State.players -> unit
 
 (** Set pot to zero*)
 val reset : unit
 
 (** Splits the pot amongs the winners. Uses a side pot if necessary.
     Assumes it is passed just the winners (one winner, ties, etc.).*)
-val to_winner : win_record list -> State.state -> int array
+val to_winner : int list -> State.state -> int array
