e770ae395650c258c5ba4efe33521d88323bf7b7
Steven Urdaneta <stevenaurdaneta@gmail.com>
2021-05-02 02:15:08 -0400

    Mli done

--- main.mli
+++ main.mli
@@ -50,13 +50,13 @@ val get_raise_amount : State.state -> int
     and then does so*)
 val prompt_action :
   State.state -> State.players array ref -> int array -> int
-
 (** Iterates through each player at the table during betting round *)
 val rec_betting_round :
   State.state ->
   State.players array ref ->
   int array ->
   int ->
+
   int
 
 (** Main function to start round of betting *)
