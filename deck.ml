04faf3f8cf36af87aaaf7f808d2307f2047fd6be
Zachary Gelfond <zmg24@cornell.edu>
2021-05-19 17:58:36 -0400

    place

--- deck.ml
+++ deck.ml
@@ -33,30 +33,30 @@ let to_deck (c_o_l : card option list) : deck = c_o_l
 let create_card (valu : value) (sut : suit) =
   { suit = sut; value = valu }
 
-let rec create_helper (deck1 : card option list) (y : int) =
-  if y < deck_size then
+let rec create_helper (deck1 : card option list) (place : int) =
+  if place < deck_size then
     let sut =
-      if y < 13 then Hearts
-      else if y < 26 then Diamonds
-      else if y < 39 then Spades
+      if place < 13 then Hearts
+      else if place < 26 then Diamonds
+      else if place < 39 then Spades
       else Clubs
     in
     let valu =
-      if y mod 13 = 0 then Two
-      else if y mod 13 = 1 then Three
-      else if y mod 13 = 2 then Four
-      else if y mod 13 = 3 then Five
-      else if y mod 13 = 4 then Six
-      else if y mod 13 = 5 then Seven
-      else if y mod 13 = 6 then Eight
-      else if y mod 13 = 7 then Nine
-      else if y mod 13 = 8 then Ten
-      else if y mod 13 = 9 then Jack
-      else if y mod 13 = 10 then Queen
-      else if y mod 13 = 11 then King
+      if place mod 13 = 0 then Two
+      else if place mod 13 = 1 then Three
+      else if place mod 13 = 2 then Four
+      else if place mod 13 = 3 then Five
+      else if place mod 13 = 4 then Six
+      else if place mod 13 = 5 then Seven
+      else if place mod 13 = 6 then Eight
+      else if place mod 13 = 7 then Nine
+      else if place mod 13 = 8 then Ten
+      else if place mod 13 = 9 then Jack
+      else if place mod 13 = 10 then Queen
+      else if place mod 13 = 11 then King
       else Ace
     in
-    Some (create_card valu sut) :: create_helper deck1 (y + 1)
+    Some (create_card valu sut) :: create_helper deck1 (place + 1)
   else to_deck deck1
 
 let create : deck = create_helper [] 0
